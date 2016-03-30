9ece79b85e7193031590ebec8320e43d4995b9df
jdaruix <jamil_salim_daruix@hotmail.com>
2016-03-30 10:33:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jdaruix/captulo6/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jdaruix.captulo6;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exemplo_wrap_content);
+    }
+}
